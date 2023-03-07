02d5c53c769bf127fc2d1197172f41efd238f4c5
iammoong <ansrlwkd225@naver.com>
2023-03-07 21:46:55 +0900

    4.0.2 update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:web_view_android/screen/home_screen.dart';
 
 void main() {
+  // Flutter 프레임워크가
+  // 앱을 실행할 준비가 될떄까지 기다린다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home : HomeScreen(),
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai';
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
+
 
   HomeScreen({Key? key}) : super(key: key);
 
@@ -18,25 +22,26 @@ class HomeScreen extends StatelessWidget {
           IconButton(onPressed: (){
             // http://
             // https://
-            if(controller == null){
-              return;
-            }
+
             // 위에 nulll일 경우를 적어놓았지만 안드로이드 스튜디오는 인식을 못하기 때문에
             // 아래처럼 느낌표를 붙여서 선언해줘야한다.
-            controller!.loadUrl(homeUrl);
+            controller.loadRequest(homeUrl);
           }, icon: Icon(
               Icons.home,
           )),
         ],
       ),
-     body: WebView(
-       //웹뷰가 생성이 되었을 떄 라는 의미
-       onWebViewCreated: (WebViewController controller){
-         this.controller = controller;
-       },
-       initialUrl: homeUrl,
-       javascriptMode: JavascriptMode.unrestricted,
+     body: WebViewWidget(
+    controller: controller,
      ),
+//웹뷰가 생성이 되었을 떄 라는 의미
+    //   onWebViewCreated: (WebViewController controller){
+    //     this.controller = controller;
+    //   },
+    //   initialUrl: homeUrl,
+    //   javascriptMode: JavascriptMode.unrestricted,
+    // ),
+     //
     );
   }
  }
